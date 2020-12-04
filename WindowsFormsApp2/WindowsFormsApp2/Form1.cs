5d0d036fcd30482a3d348f2f2447cf52f53be7ab
deko99001 <70271729+deko99001@users.noreply.github.com>
2020-12-04 09:39:57 +0100

    Add files via upload

--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -12,14 +12,46 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        Random slump = new Random();       
+        int counter = 0;
+        int slumptal;
         public Form1()
         {
             InitializeComponent();
+         slumptal = slump.Next(1, 100);
         }
 
         private void Label1_Click(object sender, EventArgs e)
         {
+            
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int tal = int.Parse(textBox1.Text);
+            if (slumptal == tal)
+                {
+                    counter++;               
+                    label3.Text = ("Det är rätt tal");    
+                }
+
+                else if (slumptal > tal)
+                {
+                    counter++;
+                    label3.Text = "Gissningen är för låg";
+            }
+                else if (slumptal < tal)
+                {
+                    label3.Text = "Gissningen är för hög";
+                    counter++;
+                    label2.Text = counter.ToString();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
